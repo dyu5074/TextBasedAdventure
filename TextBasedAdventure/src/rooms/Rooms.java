98f172659944c739314935159d76a476ea650bfa
Derek <dyu5074@bths.edu>
2017-10-30 08:45:13 -0400

    commited

--- TextBasedAdventure/src/rooms/Rooms.java
+++ TextBasedAdventure/src/rooms/Rooms.java
@@ -6,4 +6,8 @@ public class Rooms {
     private Item[] items;
     public boolean explored;
     private int x, y;
+    public static final int LEFT = 1;
+	public static final int RIGHT = 2;
+	public static final int UP = 0;
+	public static final int DOWN = 3;
 }
